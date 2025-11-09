64a26c3befbec574d3695bc042a4103bdc1882bc
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-11-09 19:47:03 +1300

    backup, moving to linux

---
+++ Core/Systems/FixedERenderingSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using ReLogic.Graphics;
+
+namespace Terramon.Core.Systems;
+
+/// <summary>
+/// Fixes the rendering of the 'é' character.
+/// The vanilla sprite fonts technically have the character in their gylph atlas,
+/// but it uses comic sans or some other font instead of Andy Bold.
+/// This makes the 'é' character map to 'e', and has a special check to draw the '`' above the e.
+/// </summary>
+public class FixedERenderingSystem : ModSystem
+{
+    private const char PokeE = 'é';
+
+    public override void Load()
+    {
+        MonoModHooks.Modify(
+            typeof(DynamicSpriteFont).GetMethod("GetCharacterData", BindingFlags.NonPublic | BindingFlags.Instance),
+            ModifyGetCharacterData
+        );
+    }
+
+    private static void ModifyGetCharacterData(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        var label = c.DefineLabel();
+        c.EmitLdarg1();
+        c.EmitLdcI4('e');
+        c.EmitCeq();
+        c.EmitBrfalse(label);
+
+        c.EmitLdarg0();
+        c.EmitLdfld(typeof(DynamicSpriteFont).GetField("_spriteCharacters", BindingFlags.Instance | BindingFlags.NonPublic)!);
+        c.EmitDelegate(static (Dictionary<char, DynamicSpriteFont.SpriteCharacterData> spriteCharacters) => spriteCharacters['e']);
+        c.EmitRet();
+        c.MarkLabel(label);
+    }
+}
